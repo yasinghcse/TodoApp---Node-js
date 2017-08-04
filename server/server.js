3dad52fbc4384f7c2a85482ecb410e31fb9d9bbf
Yadwinder <yadwinder.singh11@gmail.com>
2017-08-04 02:28:20 -0400

    Adding GET routes and its TC suite

--- server/server.js
+++ server/server.js
@@ -19,6 +19,15 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+//fetching all todos docs
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000,()=>{
   console.log('Listing at port 3000');
 });
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,11 +3,14 @@ const request = require('supertest');
 const {app}=require('./../server');
 const {Todo}=require('./../models/Todo');
 
+//create a dummy Data
+const todos=[{text: 'First Docs'},{text: 'Second Docs'}];
+
 //Cleaning Database after each request
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
-    done();
-  });
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 //Test cases for creating a todo Doc
@@ -27,7 +30,7 @@ describe('POST /todos',()=>{
       if(err){
         return done(err);
       }
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -47,11 +50,25 @@ describe('POST /todos',()=>{
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e)=>{
         done(e);
       });
     });
+  });
+});
+
+
+//TC for reading Docs
+describe('POST /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   });
 });
