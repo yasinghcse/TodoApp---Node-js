7cc1bb1ec13c02ac02a254e9e15ce01ee7ed89aa
Yadwinder <yadwinder.singh11@gmail.com>
2017-08-22 16:28:04 -0400

    Hashing of password using bcrypt lib

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,21 +1,34 @@
 const {SHA256} = require('crypto-js');
+const bcrypt= require('bcryptjs');
 
-var data= {
-  id: 10
-};
+var password= '123';
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err, hash)=>{
+    console.log(hash);
+  });
+});
 
-var token= {
-  data,
-  hash: SHA256(JSON.stringify(data)+'some salt').toString()
-};
+var hashedPassword='$2a$10$YN95KzBuO0SuJjb8uZ.RG..Op/Xn6lcBSFK45sMyvigLyfKETUoAq';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+});
 
-token.data.id=15;
-token.hash= SHA256(JSON.stringify(data)).toString();
-
-var result= SHA256(JSON.stringify(token.data)+'some salt').toString();
-if(result === token.hash){
-  console.log('Date is correct');
-}
-else{
-  console.log('Data Manipulated ....!!!');
-}
+// var data= {
+//   id: 10
+// };
+//
+// var token= {
+//   data,
+//   hash: SHA256(JSON.stringify(data)+'some salt').toString()
+// };
+//
+// token.data.id=15;
+// token.hash= SHA256(JSON.stringify(data)).toString();
+//
+// var result= SHA256(JSON.stringify(token.data)+'some salt').toString();
+// if(result === token.hash){
+//   console.log('Date is correct');
+// }
+// else{
+//   console.log('Data Manipulated ....!!!');
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose');
 const validator= require('validator');
 const jwt = require('jsonwebtoken');
 const _= require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema= new mongoose.Schema({
   email:{
@@ -67,6 +68,22 @@ UserSchema.statics.findByToken=function(token){
     'tokens.access': 'auth'
   });
 };
+
+//middleware function to handle hashed password storage using bcrpyt lib
+UserSchema.pre('save',function(next){
+  var user=this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err, hash)=>{
+        user.password=hash;
+        next();
+      });
+    });
+  }
+  else{
+    next();
+  }
+});
 
 var User=mongoose.model('User',UserSchema);
 module.exports={User};
